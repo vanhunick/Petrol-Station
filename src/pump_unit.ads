f1a867ed3cc50b71727f1376f4e064c96ce1a5b1
Nicky <nickyvanhulst@gmail.com>
2016-08-09 13:02:39 +1200

    Refactored Pump unit to meedt ada coding standards

--- src/pump_unit.ads
+++ src/pump_unit.ads
@@ -3,11 +3,10 @@ with pump; use Pump;
 with fuel_types; use fuel_types;
 with reservoir; use reservoir; use Ada.Strings.Unbounded;
 
-
 package Pump_Unit with SPARK_Mode => on is
 
-      -- States the Pump unit can be in
-   type state is  (
+   -- States the Pump unit can be in
+   type State is  (
                   BASE_STATE,
                   READY_STATE,
                   PUMPING_STATE,
@@ -18,46 +17,41 @@ package Pump_Unit with SPARK_Mode => on is
    type Pump_Unit_Type is private;
 
    -- Array of pumps the pump unit holds
-   type pumps_array is array (1..3) of Pump_Type;
+   type Pumps_Array is array (1..3) of Pump_Type;
 
    -- The reservoirs the pump unit can use for the pump types
-   type reservoir_array is array (1..3) of reservoir_type;
-
+   type Reservoir_Array is array (1..3) of Reservoir_Type;
 
     -- Creates an instance of the pump unit
-   function create(pumps : in pumps_array; res : reservoir_array)return Pump_Unit_Type;
+   function Create(Pumps : in Pumps_Array; Res : Reservoir_Array)return Pump_Unit_Type;
 
    -- Returns if the unit can start pumping
-   function can_pump(A_Pump_Unit : in Pump_Unit_Type; Amount : in Fuel_Litre) return Boolean;
+   function Can_Pump(A_Pump_Unit : in Pump_Unit_Type; Amount : in Fuel_Litre) return Boolean;
 
    -- Lift a nozzle
-   procedure lift_Nozzle(A_Pump_Unit : in out Pump_Unit_Type; ftype : in Fuel_Type); -- not sure if pump or fuel type
-
+   procedure Lift_Nozzle(A_Pump_Unit : in out Pump_Unit_Type; F_Type : in Fuel_Type); -- not sure if pump or fuel type
 
    -- Returns the nozzle to the cradle
-   procedure return_Nozzle(A_Pump_Unit : in out Pump_Unit_Type); -- not sure if pump or fuel type
+   procedure Return_Nozzle(A_Pump_Unit : in out Pump_Unit_Type); -- not sure if pump or fuel type
 
    -- Pumps fuel
-   procedure pump_fuel(A_Pump_Unit : in out Pump_Unit_Type; Amount : in Fuel_Litre);
+   procedure Pump_Fuel(A_Pump_Unit : in out Pump_Unit_Type; Amount : in Fuel_Litre);
 
    -- Check all the nozzles are down
-   function check_nozzle_down(A_pump_unit : in Pump_Unit_Type)return Boolean;
+   function Check_Nozzle_Down(A_pump_unit : in Pump_Unit_Type)return Boolean;
 
-   procedure pay_outstanding(A_Pump_Unit : in out Pump_Unit_Type); -- No need for amount
-
-   -- Pay
-
-
+   -- Pay the outstanding amount
+   procedure Pay_Outstanding(A_Pump_Unit : in out Pump_Unit_Type);
 
 private
    type Pump_Unit_Type is
       record
-         cur_state : state;
-         pumps : pumps_array;
-         reservoirs : reservoir_array;
-         pumping : Boolean;
-         outstanding : Boolean;
-         outstanding_Cost : Dollars;
+         Cur_State : State;
+         Pumps : Pumps_Array;
+         Reservoirs : Reservoir_Array;
+         Pumping : Boolean;
+         Outstanding : Boolean;
+         Outstanding_Cost : Dollars;
       end record;
 
-end pump_unit;
+end Pump_Unit;
